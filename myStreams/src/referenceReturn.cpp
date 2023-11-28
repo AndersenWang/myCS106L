459299e49f147d749e87dd279b5c7bef43e51146
AndersenWang <wangxinyu_cn@163.com>
2023-11-28 14:45:19 +0800

    Streams II

--- myStreams/src/referenceReturn.cpp
+++ myStreams/src/referenceReturn.cpp
@@ -25,24 +25,12 @@ using namespace std;
 // }
 
 // ========== Be careful about dangling references: references to variables out of scope. ==========
-// string& getRandomWord() {
-//     string word = "CS106L";
-//     return word;
-// }
-// int main(int argc, char* argv[]) {
-//     string& randomword = getRandomWord();   // word was discarded after getRandomWord() func. called  ;  you should make the copy
-//     cout << randomword << endl;
-//     return 0;
-// }
-
-struct time {
-    int hour;
-    int minute;
-    time(int hour, int minute) {
-        this->hour = hour;
-        this->minute = minute;
-    }
-};
-int main() {
+string& getRandomWord() {
+    string word = "CS106L";
+    return word;
+}
+int main(int argc, char* argv[]) {
+    string& randomword = getRandomWord();   // word was discarded after getRandomWord() func. called  ;  you should make the copy
+    cout << randomword << endl;
     return 0;
 }
